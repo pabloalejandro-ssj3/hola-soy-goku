9d73c09085f41c17c7362154f6eb9df365b87a87
pabloalejandro-ssj3 <130647241+pabloalejandro-ssj3@users.noreply.github.com>
2023-05-08 08:51:55 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,8 @@
-input.onGesture(Gesture.Shake, function () {
+input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    Número_aleatorio = randint(0, 3)
-    if (Número_aleatorio == 2) {
-        basic.showString("SI")
-    } else if (Número_aleatorio == 1) {
-        basic.showString("NO")
-    } else {
-        basic.showString("NO SE")
-    }
-    basic.showNumber(8)
+    radio.sendString("smile")
 })
-let Número_aleatorio = 0
-basic.showString("Haz una pregunta")
-basic.showNumber(8)
+radio.onReceivedString(function (receivedString) {
+    basic.showIcon(IconNames.Happy)
+})
+radio.setGroup(3)
