e7d833eafe4fe002e654b1c93cac5c41d99bbab0
pabloalejandro-ssj3 <130647241+pabloalejandro-ssj3@users.noreply.github.com>
2023-05-18 10:46:54 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
+let tool = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    if (0 == 0) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    radio.sendString("smile")
+    let mi_jugada = 0
+    basic.showIcon(IconNames.Scissors)
+    if (mi_jugada == 1) {
+        radio.sendNumber(1)
+    }
 })
-radio.onReceivedString(function (receivedString) {
-    basic.showIcon(IconNames.Happy)
+input.onGesture(Gesture.Shake, function () {
+    tool = randint(0, 2)
+    if (tool == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (tool == 1) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
 })
-radio.setGroup(3)
